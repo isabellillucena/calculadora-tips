dcf14ce9e12c469053b627e7693af5605cd1a221
Isabelli Lucena <isabelli.llucena@gmail.com>
2025-04-01 23:57:09 -0300

    criação da função para calcular todos os resultados

--- main.js
+++ main.js
@@ -5,23 +5,48 @@ let buttonSelected = null
 
 function receiveBillValue() {
     bill = document.querySelector("#bill").valueAsNumber
+
+    calculateResults()
 }
 
 function receiveNumberOfPeopleValue() {
     numberOfPeople = document.querySelector("#people").valueAsNumber
+
+    calculateResults()
 }
 
 function receiveTipPercentageValue(value) {
     tipPercentage = value / 100
 
+    document.querySelector("#costum-tip").value !== ""
+    
+    
     if (buttonSelected !== null) {
         buttonSelected.classList.remove("button-selected")
     }
 
     buttonSelected = document.querySelector(`button-${value}`)
     buttonSelected.classList.add(`button-selected`)
+
+    calculateResults()
 }
 
 function receiveCustomTipPercentageValue() {
     tipPercentage = document.querySelector("#costum-tip").valueAsNumber / 100
+
+    removeClassButtonSelected()
+
+    calculateResults()
+}
+
+function removeClassButtonSelected() {
+    
+    if (buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected = null 
+    } 
+} 
+
+function calculateResults {
+    if(bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0)
 }
