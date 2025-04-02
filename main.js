ab301a7b7534597a22287b4c58006dce68de2d34
Isabelli Lucena <isabelli.llucena@gmail.com>
2025-04-01 23:11:59 -0300

    início do javascript

---
+++ main.js
@@ -0,0 +1,12 @@
+let bill = 0
+let tip= 0
+let numberOfPeople = 0
+
+let billInput = document.querySelector("#bill")
+billInput.addEventListener("input", () => {
+    bill = billInput.valueAsNumber
+
+    console.log(bill)
+})
+
+console.log(billInput)
